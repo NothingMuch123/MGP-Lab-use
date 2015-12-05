ef35dd3dc05bb4785bbd985eeae1882ebca9eeb7
Kah Wei <xeclipse120@live.com>
2015-12-05 23:33:17 +0800

    Added Bullet class that I forgot to push previously

---
+++ app/src/main/java/kahwei/com/dm2230_mgp/Bullet.java
@@ -0,0 +1,42 @@
+package kahwei.com.dm2230_mgp;
+
+import android.graphics.Bitmap;
+
+import kahwei.com.dm2230_mgp.Object.GameObject;
+import kahwei.com.dm2230_mgp.Object.Transform;
+import kahwei.com.dm2230_mgp.Object.Vector3;
+
+/**
+ * Created by xecli on 12/5/2015.
+ */
+public class Bullet extends GameObject
+{
+    private Vector3 m_velocity;
+
+    Bullet()
+    {
+        m_velocity = new Vector3();
+        m_velocity.Set(0.0f, 0.0f, 0.0f);
+        SetActive(false);
+    }
+
+    public void Init(Bitmap mesh, boolean active, Vector3 velocity)
+    {
+        super.Init(mesh, active, active);
+        m_velocity = velocity;
+    }
+
+    public void SetVelocity(Vector3 vel)
+    {
+        m_velocity = vel;
+    }
+
+    @Override
+    public void Update(final double dt)
+    {
+        // Update the bullet position
+        Transform tf = GetTransform();
+        tf.m_translate = tf.m_translate.Add(m_velocity.ScalarMultiply((float)dt));
+        SetTransform(tf);
+    }
+}
